06fc35d2702d0b5d53e503566c0b79c77e89e7c8
Alves0611 <g.alves0611@gmail.com>
2024-09-25 23:32:09 -0300

    chore: configure S3 backend for state management

--- versions.tf
+++ versions.tf
@@ -7,4 +7,9 @@ terraform {
       version = "~> 5.22"
     }
   }
+  backend "s3" {
+    bucket = "tfstate-891377404175"
+    key    = "eks-module/dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
