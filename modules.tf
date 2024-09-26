417444bbfef2ab7c9363f835abf2e3342cc1c44a
Alves0611 <g.alves0611@gmail.com>
2024-09-26 15:00:46 -0300

    feat: pass public subnet IDs from network module to EKS cluster module

--- modules.tf
+++ modules.tf
@@ -2,13 +2,14 @@ module "eks_network" {
   source = "./modules/network"
 
   cidr_block   = var.cidr_block
-  aws_region   = var.aws_region
   project_name = var.project_name
 }
 
 module "eks_cluster" {
-  source = "./modules/cluster"
-
-  aws_region   = var.aws_region
+  source       = "./modules/cluster"
   project_name = var.project_name
+
+  public_subnet_1a = module.eks_network.public_subnet_ids[0]
+  public_subnet_1b = module.eks_network.public_subnet_ids[1]
+  public_subnet_1c = module.eks_network.public_subnet_ids[2]
 }
