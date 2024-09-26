e93eceb69d76d2e0ed0878bb562c0df7e14cec44
Alves0611 <g.alves0611@gmail.com>
2024-09-26 09:29:40 -0300

    refactor: remove separate resource files and consolidate into a single file

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -8,3 +8,28 @@ resource "aws_vpc" "this" {
     Name = "${var.project_name}-vpc"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    "Name" = "${local.namespaced_service_name}-igw"
+  }
+}
+
+resource "aws_subnet" "this" {
+  for_each = local.subnets
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = "${data.aws_region.current.name}${each.value.az}"
+  map_public_ip_on_launch = each.value.public
+
+  tags = merge(
+    {
+      "Name"                            = "${local.namespaced_service_name}-${each.value.name}",
+      "kubernetes.io/role/elb"          = each.value.public ? "1" : null,
+      "kubernetes.io/role/internal-elb" = each.value.public ? null : "1"
+    },
+    each.value.tags
+  )
+}
