512ee6f83bbc9a7b8eadb82f7cf969f4515bc140
Alves0611 <g.alves0611@gmail.com>
2024-09-26 00:57:30 -0300

    feat: add aws_region variable for configuring the deployment region

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "aws_region" {
+  type        = string
+  description = "The region to deploy to"
+}
+
 variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used for the VPC"
