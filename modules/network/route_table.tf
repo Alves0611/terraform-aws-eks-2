0eab72b12c90bbb44522d5aec688af85ebf0e97a
Alves0611 <g.alves0611@gmail.com>
2024-09-26 08:55:44 -0300

    feat: add private route tables for each availability zone with NAT Gateway associations

--- modules/network/route_table.tf
+++ modules/network/route_table.tf
@@ -13,6 +13,51 @@ resource "aws_route_table" "eks_public_route_table" {
   )
 }
 
+resource "aws_route_table" "eks_private_route_table_1a" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1a.id
+  }
+
+  tags = (
+    {
+      Name = "${var.project_name}-private-route-table-1a"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1b" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1b.id
+  }
+
+  tags = (
+    {
+      Name = "${var.project_name}-private-route-table-1b"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1c" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1b.id
+  }
+
+  tags = (
+    {
+      Name = "${var.project_name}-private-route-table-1c"
+    }
+  )
+}
+
 resource "aws_route_table_association" "eks_rtb_assoc_1a" {
   subnet_id      = aws_subnet.eks_subnet_public_1a.id
   route_table_id = aws_route_table.eks_public_route_table.id
