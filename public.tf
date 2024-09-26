f680fb7e14670844b32e90a02d2b39f960e7cc63
Alves0611 <g.alves0611@gmail.com>
2024-09-26 07:49:50 -0300

    fix: correct availability_zone format on public subnet resources

--- public.tf
+++ public.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "eks_subnet_public_1a" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "${data.aws_region.current}-a"
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -14,7 +14,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
 resource "aws_subnet" "eks_subnet_public_1b" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "${data.aws_region.current}-b"
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -26,7 +26,7 @@ resource "aws_subnet" "eks_subnet_public_1b" {
 resource "aws_subnet" "eks_subnet_public_1c" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 3)
-  availability_zone       = "${data.aws_region.current}-c"
+  availability_zone       = "${data.aws_region.current.name}c"
   map_public_ip_on_launch = true
 
   tags = {
