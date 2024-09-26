ec9303820d362b50f7845dc27bc48520f6fb430d
Alves0611 <g.alves0611@gmail.com>
2024-09-26 00:31:09 -0300

    feat: add public subnets for EKS cluster in multiple availability zones

--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,26 @@ resource "aws_vpc" "this" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
+
+
+resource "aws_subnet" "eks_subnet_public_1a" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+
+resource "aws_subnet" "eks_subnet_public_1b" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "eks_subnet_public_1c" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+}
