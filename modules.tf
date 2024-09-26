5ed07db6caffe25bf2cef7a6579ac611f8f60098
Alves0611 <g.alves0611@gmail.com>
2024-09-26 14:33:01 -0300

    feat: add EKS cluster module configuration

--- modules.tf
+++ modules.tf
@@ -1,8 +1,14 @@
 module "eks_network" {
   source = "./modules/network"
 
-  cidr_block   = "10.0.0.0/16"
-  aws_region   = "us-east-1"
-  project_name = "eks"
+  cidr_block   = var.cidr_block
+  aws_region   = var.aws_region
+  project_name = var.project_name
 }
 
+module "eks_cluster" {
+  source = "./modules/cluster"
+
+  aws_region   = var.aws_region
+  project_name = var.project_name
+}
