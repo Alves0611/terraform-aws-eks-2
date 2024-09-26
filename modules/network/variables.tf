0c02485a7ac1ccd842f344a3bba4bf3a49492bf1
Alves0611 <g.alves0611@gmail.com>
2024-09-26 14:58:46 -0300

    refactor: remove 'aws_region' variable no longer in use

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  type        = string
-  description = "The region to deploy to"
-}
-
 variable "project_name" {
   type        = string
   description = "Project name to be used to name the resources (Name tag)"
