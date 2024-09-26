8d71281f4553571f895c47334b78eb826daaf614
Alves0611 <g.alves0611@gmail.com>
2024-09-26 14:29:02 -0300

    feat: add variables for AWS region and project name

---
+++ modules/cluster/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type        = string
+  description = "The region to deploy to"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to name the resources (Name tag)"
+}
