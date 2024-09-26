6d74c581796c099e87a7b0ed0f1cc23318b0cfd7
Alves0611 <g.alves0611@gmail.com>
2024-09-26 07:53:46 -0300

    feat: add 'project_name' variable for resource naming

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "aws_region" {
   description = "The region to deploy to"
 }
 
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to name the resources (Name tag)"
+}
+
 variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used for the VPC"
