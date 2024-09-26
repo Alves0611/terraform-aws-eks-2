aafcd59d385768858f84872387dd2b0c929ec033
Alves0611 <g.alves0611@gmail.com>
2024-09-26 14:50:54 -0300

    feat: add variables for project name and public subnets in different availability zones

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -7,3 +7,18 @@ variable "project_name" {
   type        = string
   description = "Project name to be used to name the resources (Name tag)"
 }
+
+variable "public_subnet_1a" {
+  type        = string
+  description = "The ID of the public subnet in availability zone 1a"
+}
+
+variable "public_subnet_1b" {
+  type        = string
+  description = "The ID of the public subnet in availability zone 1b"
+}
+
+variable "public_subnet_1c" {
+  type        = string
+  description = "The ID of the public subnet in availability zone 1c"
+}
