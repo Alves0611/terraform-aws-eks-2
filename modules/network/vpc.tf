e8849048cd5b4795ac2bf6e8e1285040cbe5a1d5
Alves0611 <g.alves0611@gmail.com>
2024-09-26 09:32:40 -0300

    feat: add public and private route tables for VPC with internet and NAT gateway routes

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -49,3 +49,29 @@ resource "aws_nat_gateway" "this" {
     "Name" = "${local.namespaced_service_name}-nat-gateway"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    "Name" = "${local.namespaced_service_name}-public"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = {
+    "Name" = "${local.namespaced_service_name}-private"
+  }
+}
