bdee58fed5211929a3d1e12bfcb25dd0b7c9e383
Alves0611 <g.alves0611@gmail.com>
2024-09-27 08:19:29 -0300

    feat: attach AmazonEKS_CNI_Policy to EKS managed node role

--- modules/managed-node-group/iam.tf
+++ modules/managed-node-group/iam.tf
@@ -32,3 +32,8 @@ resource "aws_iam_role_policy_attachment" "eks_mng_role_attachment_ecr" {
   role       = aws_iam_role.eks_mng_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+resource "aws_iam_role_policy_attachment" "eks_mng_role_attachment_cni" {
+  role       = aws_iam_role.eks_mng_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
