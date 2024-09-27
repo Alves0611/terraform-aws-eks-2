98e3b5cbe720bd648ee68ea81226301cde6b0e73
Alves0611 <g.alves0611@gmail.com>
2024-09-27 08:26:10 -0300

    feat: add variables for private subnet IDs in multiple AZs

--- modules/managed-node-group/variables.tf
+++ modules/managed-node-group/variables.tf
@@ -8,3 +8,17 @@ variable "cluster_name" {
   description = "EKS cluster name to create MNG"
 }
 
+variable "subnet_private_1a" {
+  type        = string
+  description = "Subnet ID from AZ 1a"
+}
+
+variable "subnet_private_1b" {
+  type        = string
+  description = "Subnet ID from AZ 1b"
+}
+
+variable "subnet_private_1c" {
+  type        = string
+  description = "Subnet ID from AZ 1c"
+}
