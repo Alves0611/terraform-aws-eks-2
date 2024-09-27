a4f23cf54e5ebe90fad2a03e4a96f0d39ceb7388
Alves0611 <g.alves0611@gmail.com>
2024-09-27 08:18:38 -0300

    feat: attach AmazonEC2ContainerRegistryReadOnly policy to EKS managed node role

--- modules/managed-node-group/iam.tf
+++ modules/managed-node-group/iam.tf
@@ -27,3 +27,8 @@ resource "aws_iam_role_policy_attachment" "eks_mng_role_attachment_worker" {
   role       = aws_iam_role.eks_mng_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "eks_mng_role_attachment_ecr" {
+  role       = aws_iam_role.eks_mng_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
