59428437129f32cd9b2664a74affc71a0c3b28df
Alves0611 <g.alves0611@gmail.com>
2024-09-27 08:37:58 -0300

    feat: replace static scaling values with variables for EKS node group

--- modules/managed-node-group/mng.tf
+++ modules/managed-node-group/mng.tf
@@ -15,9 +15,9 @@ resource "aws_eks_node_group" "eks_managed_node_group" {
   )
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
   }
 
   depends_on = [
