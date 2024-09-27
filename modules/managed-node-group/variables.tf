635f6948b8d3238004c8e39a2bf6a32ef0e22219
Alves0611 <g.alves0611@gmail.com>
2024-09-27 08:25:37 -0300

    feat: add 'cluster_name' variable for EKS managed node group

--- modules/managed-node-group/variables.tf
+++ modules/managed-node-group/variables.tf
@@ -2,3 +2,9 @@ variable "project_name" {
   type        = string
   description = "Project name to be used to name the resources (Name tag)"
 }
+
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name to create MNG"
+}
+
