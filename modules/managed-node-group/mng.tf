9aae7c8bdb8dbdbfc23412bb3cc563b0d63c675d
Alves0611 <g.alves0611@gmail.com>
2024-09-27 08:37:38 -0300

    feat: add 'instance_types' variable for EKS node group

--- modules/managed-node-group/mng.tf
+++ modules/managed-node-group/mng.tf
@@ -2,11 +2,11 @@ resource "aws_eks_node_group" "eks_managed_node_group" {
   cluster_name    = var.cluster_name
   node_group_name = "${var.project_name}-nodegroup"
   node_role_arn   = aws_iam_role.eks_mng_role.arn
+  instance_types  = ["t3.medium"]
   subnet_ids = [
     var.subnet_private_1a,
     var.subnet_private_1b,
     var.subnet_private_1c
-
   ]
   tags = (
     {
