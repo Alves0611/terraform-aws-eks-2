f164b99539fe5f34bcd81f79c8078886a5bba37d
Alves0611 <g.alves0611@gmail.com>
2024-09-28 10:43:17 -0300

    feat: add Helm provider configuration for EKS cluster

--- main.tf
+++ main.tf
@@ -17,3 +17,15 @@ provider "kubernetes" {
     command     = "aws"
   }
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
+  }
+}
